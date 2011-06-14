87f5f141a1ac0bbbadafb0faf5e3d84a2f74aebd
Florian Bergmann <Bergmann.F@gmail.com>
2011-06-14 13:40:43 +0100

    Empty django project

---
+++ django_project/core_knowledge_platform/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+import views
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+        (r'^hello/', views.hello),
+    # Example:
+    # (r'^core_knowledge_platform/', include('core_knowledge_platform.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+)
