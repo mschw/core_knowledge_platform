8dc5afba4693cc41288d7e112c26694f6c26f30c
Florian Bergmann <Bergmann.F@gmail.com>
2011-06-22 14:58:37 +0100

    Added configuration for South (database migration)

--- django_project/core_knowledge_platform/settings.py
+++ django_project/core_knowledge_platform/settings.py
@@ -94,4 +94,6 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    # South is a database migration layer.
+    'south'
 )
